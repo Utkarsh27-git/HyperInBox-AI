8b3dfe59331614da14efcf1129dc572cdf33dfe5
iutkarsh27-solanki <111502301+Utkarsh27-git@users.noreply.github.com>
2024-12-11 13:00:18 +0530

    removed email sync errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import LinkAccountButton from "@/components/link-account-button"
 import { Button } from "@/components/ui/button"
 
 
 export default async function Home() {
-  return <Button > Click me to Tell you Hello lovde </Button>
+  return <LinkAccountButton/>
 }
 
